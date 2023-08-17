ed3dc211e343a3780b1a60b7c5c94aaa31bf839c
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-08-17 21:30:50 +0600

    history restore success

--- src/App.jsx
+++ src/App.jsx
@@ -1,125 +1,166 @@
+/**
+ * DONE: Handle User Input Fields
+ * Done: Handle operations
+ * DONE: Handle a list of histories
+ * DONE: Render history list
+ * TODO: Restore the history
+ */
+
 import { useState } from 'react';
-import './App.css'
 
 function* generateId() {
-  let id = 1;
+  let id = 0;
+
   while (true) {
     yield id++;
   }
-
 }
 
 const getId = generateId();
 
-
-const InitialInputState = {
+const initialInputState = {
   a: 0,
   b: 0,
 };
 
 const App = () => {
-  const [inputState, setInputState] = useState({ ...InitialInputState });
+  const [inputState, setInputState] = useState({ ...initialInputState });
   const [result, setResult] = useState(0);
-  const [historylist, setHistorylist] = useState([]);
+  const [histories, setHistories] = useState([]);
+  const [restoredHistory, setRestoredHistory] = useState(null);
 
-
-  const handleInputFields = (e) => {
+  const handleInputChange = (e) => {
     setInputState({
       ...inputState,
       [e.target.name]: parseInt(e.target.value),
-    })
-  }
+    });
+  };
 
-  const handleClear = () => {
-    setInputState({
-      ...InitialInputState
-    })
+  const handleClearOps = () => {
+    setInputState({ ...initialInputState });
     setResult(0);
-  }
+  };
 
-  const handleOperations = (Operation) => {
-
-    if (!inputState.a ||
-      !inputState.b) {
-      alert("Invalid Input");
-      return
+  const handleArithmeticOps = (operation) => {
+    if (!inputState.a || !inputState.b) {
+      alert('Invalid Input');
+      return;
     }
 
-    const fun = new Function(
+    const f = new Function(
       'operation',
-      `
-      return ${inputState.a} ${Operation} ${inputState.b}
-      `
-
-    )
-    const result = fun(Operation);
-    // console.log(fun)
-    // console.log(fun(Operation));
+      `return ${inputState.a} ${operation} ${inputState.b}`
+    );
+    const result = f(operation);
     setResult(result);
-    //TODO: 2ND OPTION
-    // setResult(eval(`${inputState.a} ${Operation} ${inputState.b}`))
 
-    const historyItem = {
+    // setResult(eval(`${inputState.a} ${operation} ${inputState.b}`));
+
+    const history = {
       id: getId.next().value,
-      inputs: inputState,
-      Operation,
+      inputs: { ...inputState },
+      operation,
       result,
       date: new Date(),
-
     };
-    setHistorylist([historyItem, ...historylist])
-    // console.log(history);
-  }
-  const handleRestore = (history) => {
-    setInputState({ ...history.inputs })
-  }
-  // const handleInputFields = (key, value) => {
+    setHistories([history, ...histories]);
+  };
 
-  // }
-  // const handleInputFields = (inp) => {
-  //   setInputState({
-  //     ...inputState,
-  //     ...inp
-  //   })
-  // }
+  const handleRestoreBtn = (history) => {
+    setInputState({ ...history.inputs });
+    setResult(history.result);
+    setRestoredHistory(history.id);
+  };
+
+  // const handleInputA = (e) => {
+  // 	setInputState({
+  // 		...inputState,
+  // 		a: parseInt(e.target.value),
+  // 	});
+  // };
+
+  // const handleInputB = (e) => {
+  // 	setInputState({
+  // 		...inputState,
+  // 		b: parseInt(e.target.value),
+  // 	});
+  // };
+
+  // const handleInputChange = (key, value) => {
+  // 	setInputState({
+  // 		...inputState,
+  // 		[key]: parseInt(value),
+  // 	});
+  // };
+
+  // const handleInputChange = (inp) => {
+  // 	setInputState({
+  // 		...inputState,
+  // 		...inp,
+  // 	});
+  // };
+
   return (
     <div style={{ width: '50%', margin: '0 auto' }}>
-      <h1>{result}</h1>
+      <h1>Result: {result}</h1>
       <div>
-        <p>Input</p>
-        <input type="number" value={inputState.a} onChange={handleInputFields} name='a' />
-        <input type="number" value={inputState.b} onChange={handleInputFields} name='b' />
+        <p>Inputs</p>
+        <input
+          type="number"
+          value={inputState.a}
+          onChange={handleInputChange}
+          name="a"
+        />
+        <input
+          type="number"
+          value={inputState.b}
+          onChange={handleInputChange}
+          name="b"
+        />
       </div>
       <div>
         <p>Operations</p>
-        <button onClick={() => handleOperations('+')}>+</button>
-        <button onClick={() => handleOperations('-')}>-</button>
-        <button onClick={() => handleOperations('*')}>*</button>
-        <button onClick={() => handleOperations('/')}>/</button>
-        <button onClick={handleClear}>reset</button>
+        <button onClick={() => handleArithmeticOps('+')}>+</button>
+        <button onClick={() => handleArithmeticOps('-')}>-</button>
+        <button onClick={() => handleArithmeticOps('*')}>*</button>
+        <button onClick={() => handleArithmeticOps('/')}>/</button>
+        <button onClick={() => handleArithmeticOps('%')}>%</button>
+        <button onClick={handleClearOps}>Clear</button>
       </div>
       <div>
         <p>History</p>
-        {historylist.length == 0 ? <p>
-          <small>There is no history</small>
-        </p> : <ul>
-          {historylist.map((historyItem) => (
-            <li key={historyItem.id}>
-              <p>Operation: {historyItem.inputs.a} {historyItem.Operation} {historyItem.inputs.b}, Result: {historyItem.result}</p>
-              <small>{historyItem.date.toLocaleDateString()}{' '}{historyItem.date.toLocaleTimeString()}</small>
-              <br />
-              <button onClick={() => handleRestore(history)}>
-                restore
-              </button>
-            </li>
-          ))}
-
-        </ul>}
-
-
+        {histories.length === 0 ? (
+          <p>
+            <small>There is no history</small>
+          </p>
+        ) : (
+          <ul>
+            {histories.map((history) => (
+              <li key={history.id}>
+                <p>
+                  Operations: {history.inputs.a} {history.operation}{' '}
+                  {history.inputs.b}, Result = {history.result}
+                </p>
+                <small>
+                  {history.date.toLocaleDateString()}{' '}
+                  {history.date.toLocaleTimeString()}
+                </small>
+                <br />
+                <button
+                  onClick={() => handleRestoreBtn(history)}
+                  disabled={
+                    restoredHistory !== null && restoredHistory === history.id
+                  }
+                >
+                  Restore
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
